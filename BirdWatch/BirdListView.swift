fc2ecfa9e4d32b5a20bb05045474d6be5ff7d864
Vin Bui <vdb23@cornell.edu>
2023-04-12 16:46:56 -0400

    Finish 2-refactor

---
+++ BirdWatch/BirdListView.swift
@@ -0,0 +1,49 @@
+//
+//  BirdListView.swift
+//  BirdWatch
+//
+//  Created by Vin Bui on 4/12/23.
+//
+
+import SwiftUI
+
+struct BirdListView: View {
+    
+    var body: some View {
+        List(birds, id: \.self) { bird in
+            birdInfoRow(name: bird.name, location: bird.location, image: bird.image)
+        }
+    }
+    
+    private func birdInfoRow(name: String, location: String, image: String) -> some View {
+        HStack {
+            Spacer()
+            
+            VStack(alignment: .leading) {
+                Text(name)
+                    .fontWeight(.bold)
+                
+                Text(location)
+                    .fontWeight(.medium)
+                    .italic()
+            }
+            .font(.title)
+            
+            Spacer()
+            
+            Image(image)
+                .resizable()
+                .frame(width: 100, height: 100)
+            
+            Spacer()
+        }
+    }
+    
+}
+
+struct BirdListView_Previews: PreviewProvider {
+    static var previews: some View {
+        BirdListView()
+    }
+}
+
--- BirdWatch/ContentView.swift
+++ BirdWatch/ContentView.swift
@@ -8,35 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        List(birds, id: \.self) { bird in
-            birdInfoRow(name: bird.name, location: bird.location, image: bird.image)
-        }
+        BirdListView()
     }
     
-    private func birdInfoRow(name: String, location: String, image: String) -> some View {
-        HStack {
-            Spacer()
-            
-            VStack(alignment: .leading) {
-                Text(name)
-                    .fontWeight(.bold)
-                
-                Text(location)
-                    .fontWeight(.medium)
-                    .italic()
-            }
-            .font(.title)
-            
-            Spacer()
-            
-            Image(image)
-                .resizable()
-                .frame(width: 100, height: 100)
-            
-            Spacer()
-        }
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
