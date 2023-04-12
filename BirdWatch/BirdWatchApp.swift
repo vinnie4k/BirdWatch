7a7041dadcb658b2e11c8b31172418fe1678fbb7
Vin Bui <vdb23@cornell.edu>
2023-04-12 15:45:40 -0400

    Source code
    - Added dummy data
    - Added image assets

---
+++ BirdWatch/BirdWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdWatchApp.swift
+//  BirdWatch
+//
+//  Created by Vin Bui on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BirdWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
