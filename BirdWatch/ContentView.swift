508f0c6e1584b608ab6a98dd4d7afab1beb23e2e
Vin Bui <vdb23@cornell.edu>
2023-04-12 16:16:38 -0400

    Finish 1-layouts

--- BirdWatch/ContentView.swift
+++ BirdWatch/ContentView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            Spacer()
+            
+            VStack(alignment: .leading) {
+                Text("Vin")
+                    .fontWeight(.bold)
+                
+                Text("Texas")
+                    .fontWeight(.medium)
+                    .italic()
+            }
+            .font(.title)
+            
+            Spacer()
+            
+            Image("vin")
+                .resizable()
+                .frame(width: 100, height: 100)
+            
+            Spacer()
+        }
     }
 }
 
