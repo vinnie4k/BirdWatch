bfd3f63091625b14251e8c4ee3555bc1fe322d4c
Vin Bui <vdb23@cornell.edu>
2023-04-13 12:04:44 -0400

    Change layout

--- BirdWatch/ContentView.swift
+++ BirdWatch/ContentView.swift
@@ -10,13 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         List(birds, id: \.self) { bird in
-            birdInfoRow(name: bird.name, location: bird.location, image: bird.image)
+            birdInfoRow(name: bird.name, location: bird.location, image: bird.image, count: bird.count)
         }
     }
     
-    private func birdInfoRow(name: String, location: String, image: String) -> some View {
+    private func birdInfoRow(name: String, location: String, image: String, count: Int) -> some View {
         HStack {
-            Spacer()
+            Text(String(count))
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.trailing, 30)
             
             VStack(alignment: .leading) {
                 Text(name)
@@ -26,15 +29,13 @@ struct ContentView: View {
                     .fontWeight(.medium)
                     .italic()
             }
-            .font(.title)
+            .font(.title2)
             
             Spacer()
             
             Image(image)
                 .resizable()
                 .frame(width: 100, height: 100)
-            
-            Spacer()
         }
     }
 }
